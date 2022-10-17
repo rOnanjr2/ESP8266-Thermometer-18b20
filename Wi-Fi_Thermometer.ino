fdb04c362354c9c707e4468aca9b21efd7edd182
rOnanjr2 <ronanjr2@mail.ru>
2022-10-18 00:34:13 +0300

    add timeline

---
+++ Wi-Fi_Thermometer.ino
@@ -0,0 +1,202 @@
+#include <ESP8266WiFi.h>                                                // Библиотека для создания Wi-Fi подключения (клиент или точка доступа)
+#include <ESP8266WebServer.h>                                           // Библиотека для управления устройством по HTTP (например из браузера)
+#include <FS.h>                                                         // Библиотека для работы с файловой системой
+#include <ESP8266FtpServer.h>                                           // Библиотека для работы с SPIFFS по FTP
+#include <WiFiClient.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+#ifndef STASSID
+#define STASSID "Wi-Fi"
+#define STAPSK  "renegade"
+#endif
+
+const char *ssid = STASSID;
+const char *password = STAPSK;
+
+const int oneWireBus = 13;    
+unsigned long timing;
+float temperatureC;
+
+
+OneWire oneWire(oneWireBus);
+DallasTemperature sensors(&oneWire);
+
+#define RELAY LED_BUILTIN                                               // Пин подключения сигнального контакта реле
+#define TEMP_REFRESH_PERIOD 10 //5
+#define GRAPH_WRITE_DIVIDER 6 //6
+#define GRAPH_LEN 2880 //300
+
+
+int graphData[GRAPH_LEN];
+
+uint16_t graphPointer;
+uint16_t sendPointer;
+
+byte graphWriteDivider;
+
+ESP8266WebServer HTTP(80);                                              // Определяем объект и порт сервера для работы с HTTP
+FtpServer ftpSrv;                                                       // Определяем объект для работы с модулем по FTP (для отладки HTML)
+
+void setup() {
+
+  pinMode(RELAY, OUTPUT);                                                // Определяем пин реле как исходящий
+  Serial.begin(9600);    
+  
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  Serial.println("");
+
+
+  // for (int i = 0; i < GRAPH_LEN; ++i) {
+  //   graphData[i] = 2000;
+  // };
+
+
+  // Wait for connection
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.print("Connected to ");
+  Serial.println(ssid);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());                                       // Инициализируем вывод данных на серийный порт со скоростью 9600 бод
+  SPIFFS.begin();                                                       // Инициализируем работу с файловой системой                          
+  HTTP.begin();                                                         // Инициализируем Web-сервер
+  ftpSrv.begin("relay","relay");                                        // Поднимаем FTP-сервер для удобства отладки работы HTML (логин: relay, пароль: relay)
+  sensors.begin();
+
+
+  // HTTP.on("/relay_switch", [](){                                        // При HTTP запросе вида http://192.168.4.1/relay_switch
+  //     HTTP.send(200, "text/plain", relay_switch());                     // Отдаём клиенту код успешной обработки запроса, сообщаем, что формат ответа текстовый и возвращаем результат выполнения функции relay_switch 
+  //   });
+
+
+  // HTTP.on("/relay_status", [](){                                        // При HTTP запросе вида http://192.168.4.1/relay_status
+  //     HTTP.send(200, "text/plain", relay_status());                     // Отдаём клиенту код успешной обработки запроса, сообщаем, что формат ответа текстовый и возвращаем результат выполнения функции relay_status 
+  //   });
+
+  HTTP.on("/temperature", [](){                                         // При HTTP запросе вида http://192.168.4.1/temperature
+      HTTP.send(200, "text/plain", String(temperatureC));               // Отдаём клиенту код успешной обработки запроса, сообщаем, что формат ответа текстовый и возвращаем температуру
+    });
+
+  HTTP.on("/graph", [](){                                               // При HTTP запросе вида http://192.168.4.1/graph
+      HTTP.send(200, "text/plain", sendGraph());                        // Отдаём клиенту код успешной обработки запроса, сообщаем, что формат ответа текстовый и возвращаем массив с графиком
+    });
+
+
+
+  HTTP.onNotFound([](){                                                 // Описываем действия при событии "Не найдено"
+  if(!handleFileRead(HTTP.uri()))                                       // Если функция handleFileRead (описана ниже) возвращает значение false в ответ на поиск файла в файловой системе
+      HTTP.send(404, "text/plain", "Not Found");                        // возвращаем на запрос текстовое сообщение "File isn't found" с кодом 404 (не найдено)
+  });
+}
+
+void loop() {
+    // digitalWrite(RELAY, 1);
+    HTTP.handleClient();                                                // Обработчик HTTP-событий (отлавливает HTTP-запросы к устройству и обрабатывает их в соответствии с выше описанным алгоритмом)
+    ftpSrv.handleFTP();                                                 // Обработчик FTP-соединений  
+    tempCheck(TEMP_REFRESH_PERIOD);
+    // digitalWrite(RELAY, 0);
+  }
+
+
+
+
+void tempCheck(unsigned int t) {
+      digitalWrite(RELAY, 0);
+  if (millis() - timing > t * 1000){                                     // Проверяем миллис на прошедшее время
+    timing = millis();                                                   // Обновляем время прошлого срабатывания
+    timing = timing - (timing % 10);                                     // Округляем тайминг для большей точности
+    sensors.requestTemperatures();                                       // Запрашиваем вычисление температуры
+    temperatureC = sensors.getTempCByIndex(0);                           // Получаем значение температуры в переменную
+    if (graphWriteDivider < GRAPH_WRITE_DIVIDER - 1)
+    {
+      graphWriteDivider++;
+    }else{
+      writeGraph();
+      graphWriteDivider = 0;
+    }
+  }
+      digitalWrite(RELAY, 1);
+}
+
+
+
+String sendGraph() {
+  String str;
+  for (int i = 0; i < sendPointer; ++i)
+  {
+    str += graphData[i];
+    if (i < sendPointer - 1)
+    {
+      str += ",";
+    }
+  }
+  return str + "," + 
+  graphPointer + "," + 
+  TEMP_REFRESH_PERIOD + "," + 
+  GRAPH_WRITE_DIVIDER;
+}
+
+
+
+void writeGraph() {
+  if (graphPointer < GRAPH_LEN - 1)
+    {
+      graphData[graphPointer] = int(temperatureC*10);
+      graphPointer++;
+      if (sendPointer < GRAPH_LEN)
+      {
+        sendPointer++;
+      }
+    }else{
+      graphData[graphPointer] = int(temperatureC*10);
+      graphPointer = 0;
+    }
+}
+
+// String relay_switch() {                                                 // Функция переключения реле 
+//   byte state;
+//   if (digitalRead(RELAY))                                               // Если на пине реле высокий уровень   
+//     state = 0;                                                          //  то запоминаем, что его надо поменять на низкий
+//   else                                                                  // иначе
+//     state = 1;                                                          //  запоминаем, что надо поменять на высокий
+//   digitalWrite(RELAY, state);                                           // меняем значение на пине подключения реле
+//   return String(state);                                                 // возвращаем результат, преобразовав число в строку
+// }
+
+// String relay_status() {                                                 // Функция для определения текущего статуса реле 
+//   byte state;
+//   if (digitalRead(RELAY))                                               // Если на пине реле высокий уровень   
+//     state = 1;                                                          //  то запоминаем его как единицу
+//   else                                                                  // иначе
+//     state = 0;                                                          //  запоминаем его как ноль
+//   return String(state);                                                 // возвращаем результат, преобразовав число в строку
+// }
+
+bool handleFileRead(String path){                                       // Функция работы с файловой системой
+  if(path.endsWith("/")) path += "index.html";                          // Если устройство вызывается по корневому адресу, то должен вызываться файл index.html (добавляем его в конец адреса)
+  String contentType = getContentType(path);                            // С помощью функции getContentType (описана ниже) определяем по типу файла (в адресе обращения) какой заголовок необходимо возвращать по его вызову
+  if(SPIFFS.exists(path)){                                              // Если в файловой системе существует файл по адресу обращения
+    File file = SPIFFS.open(path, "r");                                 //  Открываем файл для чтения
+    size_t sent = HTTP.streamFile(file, contentType);                   //  Выводим содержимое файла по HTTP, указывая заголовок типа содержимого contentType
+    file.close();                                                       //  Закрываем файл
+    return true;                                                        //  Завершаем выполнение функции, возвращая результатом ее исполнения true (истина)
+  }
+  return false;                                                         // Завершаем выполнение функции, возвращая результатом ее исполнения false (если не обработалось предыдущее условие)
+}
+
+String getContentType(String filename){                                 // Функция, возвращающая необходимый заголовок типа содержимого в зависимости от расширения файла
+  if (filename.endsWith(".html")) return "text/html";                   // Если файл заканчивается на ".html", то возвращаем заголовок "text/html" и завершаем выполнение функции
+  else if (filename.endsWith(".css")) return "text/css";                // Если файл заканчивается на ".css", то возвращаем заголовок "text/css" и завершаем выполнение функции
+  else if (filename.endsWith(".js")) return "application/javascript";   // Если файл заканчивается на ".js", то возвращаем заголовок "application/javascript" и завершаем выполнение функции
+  else if (filename.endsWith(".png")) return "image/png";               // Если файл заканчивается на ".png", то возвращаем заголовок "image/png" и завершаем выполнение функции
+  else if (filename.endsWith(".jpg")) return "image/jpeg";              // Если файл заканчивается на ".jpg", то возвращаем заголовок "image/jpg" и завершаем выполнение функции
+  else if (filename.endsWith(".gif")) return "image/gif";               // Если файл заканчивается на ".gif", то возвращаем заголовок "image/gif" и завершаем выполнение функции
+  else if (filename.endsWith(".ico")) return "image/x-icon";            // Если файл заканчивается на ".ico", то возвращаем заголовок "image/x-icon" и завершаем выполнение функции
+  return "text/plain";                                                  // Если ни один из типов файла не совпал, то считаем что содержимое файла текстовое, отдаем соответствующий заголовок и завершаем выполнение функции
+}
--- script.js
+++ script.js
@@ -1,145 +1,263 @@
-'use strict';
+"use strict";
 
-const settings = {
-  graphRefreshPeriod: 5,      //seconds
-  tempRefreshPeriod:  2,      //seconds
-  graphYAxisMin:      0,     //degrees
-  graphYAxisMax:      30,     //degrees
-  graphYAxisDecimals: 0,      //digits
-}
+const graphStr = "71,71,71,71,71,70,70,70,70,69,70,69,70,69,70,69,70,70,70,70,70,69,68,68,68,68,69,68,68,68,68,68,68,68,68,68,68,68,67,68,68,68,68,67,66,66,66,66,67,66,66,66,66,66,66,66,65,65,64,64,65,64,64,63,63,63,63,63,63,63,63,62,62,61,61,60,60,60,61,60,60,59,60,60,58,58,57,57,56,57,58,58,58,58,58,57,58,57,57,57,58,57,57,56,57,56,56,56,56,56,56,55,56,56,56,55,55,54,55,55,55,55,55,55,55,55,55,55,55,55,55,55,55,55,54,55,55,54,55,54,55,54,54,54,55,54,54,54,54,53,55,55,55,55,55,54,54,55,55,55,54,54,55,54,55,55,54,53,53,53,53,53,53,53,53,53,53,54,53,53,53,53,53,52,53,53,54,54,54,54,54,55,55,55,55,55,55,55,55,55,55,56,56,56,56,56,55,55,55,55,55,55,55,54,55,55,54,54,54,53,53,54,54,53,53,53,53,52,52,51,51,51,51,51,51,51,51,51,51,51,51,50,50,50,50,50,49,49,50,48,48,48,48,48,47,48,48,48,48,48,49,48,48,49,49,50,49,49,50,49,48,49,49,50,50,50,50,51,49,49,49,50,50,50,50,49,49,49,48,49,48,48,48,49,49,49,49,49,48,48,48,48,48,48,48,48,48,48,48,48,48,48,48,47,46,47,46,46,46,48,47,47,48,48,48,48,48,48,48,48,48,48,48,48,47,47,48,47,47,48,48,46,46,46,46,46,46,46,46,46,46,46,45,45,45,46,45,45,45,45,45,44,45,45,44,45,44,44,44,44,44,44,44,43,44,45,44,43,43,43,43,43,43,42,43,44,44,44,44,45,45,45,45,45,45,44,43,43,43,41,41,41,41,41,41,41,41,41,41,41,41,40,40,40,40,38,38,38,40,39,39,39,39,40,40,40,40,40,40,40,40,40,40,40,40,39,40,40,40,40,40,39,39,38,39,39,38,39,38,38,38,39,38,38,38,38,38,38,38,38,38,38,38,38,38,38,38,37,37,37,37,36,36,36,36,36,36,36,36,36,35,35,35,35,35,35,35,34,35,35,34,33,33,33,33,33,33,33,33,33,33,33,33,32,33,32,32,32,31,32,31,31,31,31,32,31,31,31,30,30,30,30,30,30,30,29,29,28,28,28,30,29,29,30,29,30,31,31,31,31,31,30,31,31,31,31,31,30,31,31,30,30,30,30,30,30,31,31,31,31,31,31,31,31,32,32,31,31,32,31,32,32,32,33,33,33,33,33,33,33,33,33,33,33,33,34,34,35,35,36,36,35,35,35,35,36,36,37,38,38,38,38,38,38,38,38,39,40,40,39,40,40,40,41,41,42,42,42,43,43,43,43,43,45,45,45,46,46,46,46,47,47,48,49,49,50,49,50,50,51,51,51,53,52,52,53,54,54,54,55,55,55,56,56,56,56,57,57,58,58,58,59,59,60,60,60,60,61,61,63,63,63,63,64,64,65,65,66,66,66,66,67,67,67,68,68,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,686";
+const tempStr = "22.50";
 
-let dataset = [];
-const datasetShifted = [];
-let graphPointer;
+let datesTempArr = [[2]];
 
-const opts = {
- angle: -0.25,
+(function () {
+  const settings = {
+    graphRefreshPeriod: 60, //seconds
+    tempRefresh: 10, //seconds
+    graphYAxisMin: 0, //degrees
+    graphYAxisMax: 30, //degrees
+    graphYAxisDecimals: 0, //digits
+  };
+
+  let dataset = [];
+  const datasetShifted = [];
+  // let graphPointer;
+
+  const opts = {
+    angle: -0.25,
     lineWidth: 0.2,
-    radiusScale:0.9,
+    radiusScale: 0.9,
     pointer: {
       length: 0.7,
       strokeWidth: 0.05,
-      color: '#000000'
+      color: "#000000",
     },
     staticLabels: {
       font: "14px sans-serif",
-      labels: [20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100],
-      fractionDigits: 0
+      labels: [
+      20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100,
+      ],
+      fractionDigits: 0,
+      // color:"red"
     },
     staticZones: [
-       {strokeStyle: "#F2F2F2", min: 0, max: 40},
-       {strokeStyle: "#FFDD00", min: 40, max: 60},
-       {strokeStyle: "#FFC300", min: 60, max: 75},
-       {strokeStyle: "#30B32D", min: 75, max: 95},
-       {strokeStyle: "#F03E3E", min: 95, max: 100}
+    { strokeStyle: "#F2F2F2", min: 0, max: 40 },
+    { strokeStyle: "#FFDD00", min: 40, max: 60 },
+    { strokeStyle: "#FFC300", min: 60, max: 75 },
+    { strokeStyle: "#30B32D", min: 75, max: 95 },
+    { strokeStyle: "#F03E3E", min: 95, max: 100 },
     ],
     limitMax: false,
     limitMin: false,
-    highDpiSupport: true
-};
-const target = document.getElementById('demo'); // your canvas element
-const gauge = new Gauge(target).setOptions(opts); // create sexy gauge!
-gauge.maxValue = 100; // set max gauge value
-gauge.setMinValue(0);  // set min value
-gauge.set(0); // set actual value
+    highDpiSupport: true,
+  };
+  const target = document.getElementById("demo"); // your canvas element
+  const gauge = new Gauge(target).setOptions(opts); // create sexy gauge!
+  gauge.maxValue = 100; // set max gauge value
+  gauge.setMinValue(0); // set min value
+  gauge.set(0); // set actual value
 
-const options = {
-  series: [
+  const apexchartsOptions = {
+    series: [
     {
       name: "Temperature",
       data: dataset,
     },
-  ],
-  chart: {
-    height: 300,
-    type: "area",
-    background: "#fff",
-  },
-  dataLabels: {
-    enabled: false,
-  },
-  stroke: {
-    curve: "straight",
-    width: 1,
-  },
-  colors: ["#008ffb"],
-  tooltip: {
-    enabled: true,
-  },
-  xaxis: {
-    type: "numeric",
-    min: 0,
-    decimalsInFloat: settings.graphYAxisDecimals,
-    tickAmount: 10,
-    hideOverlappingLabels: true,
-    labels: { style: { cssClass: "apexcharts-xaxis-label" } },
-  },
-  yaxis: {
-  tickAmount: 6,
-  // min: settings.graphYAxisMin,
-  // max: settings.graphYAxisMax,
-  decimalsInFloat: 0,
-    labels: { style: { cssClass: "apexcharts-yaxis-label" } },
-  },
-};
-const chart = new ApexCharts(document.querySelector("#chart"), options);
-chart.render();
+    ],
+    chart: {
+      width: "100%",
+      height: '60%',
+      type: "area",
+      background: "#fff",
+    },
+    dataLabels: {
+      enabled: false,
+    },
+    stroke: {
+      curve: "straight",
+      width: 1,
+    },
+    colors: ["#008ffb"],
+    tooltip: {
+      enabled: true,
+      x: {
+        format: 'HH:mm'
+      }
+    },
+    xaxis: {
+      type: "datetime",
+      // min: 0,
+      // decimalsInFloat: settings.graphYAxisDecimals,
+      // tickAmount: 10,
+      // hideOverlappingLabels: true,
+      labels: { style: { cssClass: "apexcharts-xaxis-label" } },
+    },
+    yaxis: {
+      tickAmount: 6,
+      // min: settings.graphYAxisMin,
+      // max: settings.graphYAxisMax,
+      decimalsInFloat: 0,
+      labels: { style: { cssClass: "apexcharts-yaxis-label" } },
+    },
+    theme: {
+      mode: "light",
+    },
+  };
+  const chart = new ApexCharts(
+    document.querySelector("#chart"),
+    apexchartsOptions
+    );
+  chart.render();
 
-
-const shift = () => {
-  let is = graphPointer;
-  for (let i = 0; i < dataset.length; i++) {
-    if (is < dataset.length) {
-      datasetShifted[i] = dataset[is];
-      is++;
-    }else{
-      datasetShifted[i] = dataset[is - dataset.length];
-      is++;
+  const shift = (graphPointer) => {
+    let is = graphPointer;
+    for (let i = 0; i < dataset.length; i++) {
+      if (is < dataset.length) {
+        datasetShifted[i] = dataset[is];
+        is++;
+      } else {
+        datasetShifted[i] = dataset[is - dataset.length];
+        is++;
+      }
     }
-  }
-};
+  };
 
+  // const popZeroes = () => {
+  //   while (dataset[dataset.length - 1] == 200) {
+  //     dataset.pop();
+  //     // console.log(dataset);
+  //   }
+  // };
 
-const draw_temp = () => {
-  const request = new XMLHttpRequest();
-  request.open('GET', '/temperature', true);
-  request.onload = function() {
-    if (request.readyState == 4 && request.status == 200) {
-      const response = request.responseText;
-      const element = document.getElementById('temp_text');
-      element.innerHTML = response;
-      gauge.set(response);
+  const appendDates = (graphPointPeriod) => {
+    const now = new Date();
+    for (let i = 0; i < datasetShifted.length; i++) {
+      datesTempArr[i] = [now.getTime() + 10800000 - (datasetShifted.length - i) * graphPointPeriod * 1000,
+      datasetShifted[i]];
     }
+  };
+
+  const drawTemp = (response) => {
+    const element = document.getElementById("temp_text");
+    element.innerHTML = response;
+    gauge.set(response);
+  };
+
+  const requestTemp = () => {
+    const request = new XMLHttpRequest();
+    request.open("GET", "/temperature", true);
+    request.onload = function () {
+      if (request.readyState == 4 && request.status == 200) {
+        drawTemp(request.responseText);
+      }
+    };
+    request.send();
+  };
+
+  const drawGraph = (response) => {
+    dataset = response.split(",").map(function (str) {
+      return parseInt(str) / 10;
+    });
+    let graphWriteDivider = Number(dataset.pop()) * 10;
+    let tempRefreshPeriod = Number(dataset.pop()) * 10;
+    let graphPointer = Number(dataset.pop()) * 10;
+    shift(graphPointer);
+    appendDates(graphWriteDivider * tempRefreshPeriod);
+    chart.updateSeries([{ data: datesTempArr }]);
   }
-  request.send();
-};
 
+  const requestGraph = () => {
+    const request = new XMLHttpRequest();
+    request.open("GET", "/graph", true);
+    request.onload = function () {
+      if (request.readyState == 4 && request.status == 200) {
+        drawGraph(request.responseText);
+      }
+    };
+    request.send();
+  };
 
-const draw_graph = () => {
-  const request = new XMLHttpRequest();
-  request.open('GET', '/graph', true);
-  request.onload = function() {
-    if (request.readyState == 4 && request.status == 200) {
-      const response = request.responseText;
-      dataset = response.split(',').map(function(str) {
-         return parseInt(str) / 10;
-       });
-      graphPointer = Number(dataset.pop()) * 10;
-      shift();
-      chart.updateSeries([{data: datasetShifted}])
+  const refresh = () => {
+    requestTemp();
+    requestGraph();
+    // drawGraph(graphStr);
+    // drawTemp(tempStr);
+  };
+
+  setInterval(requestTemp, settings.tempRefresh * 1000);
+  setInterval(requestGraph, settings.graphRefreshPeriod * 1000);
+
+  document.addEventListener("DOMContentLoaded", refresh);
+
+  // ############################# темы здесь
+  const storageKey = "dark-theme-choosed";
+  let darkTheme = false;
+  let themeBtn;
+  let docBody;
+
+  window.onload = () => {
+    themeBtn = document.querySelector("#theme-toggle");
+    themeBtn.addEventListener("click", themeToggle);
+    docBody = document.querySelector("body");
+
+    //чтение из  локального хранилища (оно сохранит данные если обновить вкладку)
+    if (localStorage.getItem(storageKey)) {
+      darkTheme = localStorage.getItem(storageKey);
+      darkTheme = darkTheme === "true" ? true : false;
+      console.log(darkTheme);
+      console.log(opts);
+      if (darkTheme) {
+        docBody.classList.toggle("dark");
+
+        //####################### изменение цветов шкалы
+        opts.pointer.color = "#dadada";
+        opts.staticLabels.color="#dadada";
+        gauge.setOptions(opts);
+
+        //####################### задание темы графика
+        chart.updateOptions({
+          theme: {
+            mode: "dark",
+          },
+          chart: { background: "#101010" },
+        });
+      }
     }
-  }
-  request.send();
-};
+  };
 
+  const themeToggle = () => {
+    // console.log(darkTheme);
+    docBody.classList.toggle("dark");
+    darkTheme = !darkTheme;
+    saveStateToLocalStorage();
 
+    if (docBody.classList.contains("dark")) {
+      //####################### изменение цветов шкалы
 
-const refresh = () => {
-  draw_temp();
-  draw_graph();
-};
+      opts.pointer.color = "#dadada";
+      opts.staticLabels.color="#dadada";
+      gauge.setOptions(opts);
 
-setInterval (draw_temp, settings.tempRefreshPeriod * 1000);
-setInterval (draw_graph, settings.graphRefreshPeriod * 1000);
+      //https://apexcharts.com/docs/methods/#updateOptions
+      chart.updateOptions({
+        theme: {
+          mode: "dark",
+        },
+        chart: { background: "#101010" },
+      });
+    } else {
+      //####################### изменение цветов шкалы
 
-document.addEventListener('DOMContentLoaded', refresh);
+      opts.pointer.color = "#000";
+      opts.staticLabels.color="#000";
+      gauge.setOptions(opts);
+
+      // apexchartsOptions.theme.mode = "light";
+      // apexchartsOptions.chart.background = "#fff";
+      chart.updateOptions({
+        theme: {
+          mode: "light",
+        },
+        chart: { background: "#fff" },
+      });
+    }
+  };
+  const saveStateToLocalStorage = () => {
+    localStorage.setItem(storageKey, darkTheme);
+  };
+}.call(this));
