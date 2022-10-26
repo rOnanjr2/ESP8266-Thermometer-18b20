59c1c48b12df872116344c65be54e3117906c879
rOnanjr2 <ronanjr2@mail.ru>
2022-10-26 22:13:27 +0300

    gauge mode fix

--- script.js
+++ script.js
@@ -7,7 +7,7 @@
   const graphStr =
     "156,158,157,156,157,156,158,156,156,157,158,160,160,160,162,163,160,161,160,161,161,159,162,165,165,166,165,163,164,165,166,168,166,165,165,167,169,171,171,169,170,168,166,168,166,168,167,168,178,189,205,209,210,211,213,210,213,213,221,214,218,215,220,218,221,222,223,230,235,235,231,222,221,220,221,225,232,228,230,236,226,226,230,225,225,227,225,226,226,228,228,228,224,221,223,225,230,225,226,228,226,224,230,228,225,224,230,230,230,231,226,229,223,220,225,227,230,228,233,233,230,230,236,234,239,239,245,241,236,232,236,238,238,238,240,237,240,227,225,228,238,241,236,236,230,220,219,225,225,225,228,231,228,233,231,233,235,242,251,248,241,247,238,235,236,242,237,231,234,240,238,233,238,233,234,235,230,226,225,221,223,222,210,201,198,204,217,231,237,231,238,237,237,245,245,243,236,240,243,236,240,243,238,241,238,238,238,240,245,240,231,235,235,230,235,235,236,237,236,232,230,236,236,230,231,225,224,216,210,199,193,190,187,185,183,182,182,181,180,180,180,179,179,179,179,179,177,177,176,176,176,176,176,176,175,175,175,175,173,173,173,173,172,172,171,171,171,170,171,170,169,168,168,168,167,166,165,163,163,163,160,156,151,150,145,142,140,136,135,135,133,131,130,130,128,128,126,126,126,126,124,123,121,120,120,119,118,117,115,115,113,113,113,112,111,111,111,110,110,109,108,107,107,106,106,105,105,104,104,103,101,101,101,100,100,100,98,97,97,96,96,95,95,95,95,94,92,91,91,91,91,91,91,91,90,90,89,89,88,89,89,89,89,89,89,88,87,86,86,85,84,85,84,82,83,82,81,81,82,82,82,82,83,82,82,82,82,81,81,81,82,82,81,81,81,80,79,80,80,80,80,80,80,80,80,79,79,79,79,79,79,79,78,78,78,78,78,78,78,78,79,78,78,78,77,77,77,76,76,76,76,75,75,75,75,75,75,74,73,73,72,71,71,71,71,71,71,70,70,70,70,69,69,69,69,69,69,69,69,69,69,69,69,69,69,68,68,68,68,68,66,66,66,65,64,65,65,65,65,66,66,66,65,66,66,65,65,65,65,65,65,64,65,65,65,65,64,65,64,65,65,64,64,63,63,64,65,64,63,64,63,63,63,63,63,64,64,64,63,63,64,63,65,523,10,6";
   const tempStr = "22.53";
-  const tempNum = parseFloat(tempStr);
+  let tempNum = parseFloat(tempStr);
 
   // ********************************************
   //  offline demo section end
@@ -26,8 +26,6 @@
 
   let dataset = [];
   const datasetShifted = [];
-  // let graphPointer;
-
 
   const apexchartsOptions = {
     series: [
@@ -58,10 +56,6 @@
     },
     xaxis: {
       type: "datetime",
-      // min: 0,
-      // decimalsInFloat: settings.graphYAxisDecimals,
-      // tickAmount: 10,
-      // hideOverlappingLabels: true,
       labels: { style: { cssClass: "apexcharts-xaxis-label" } },
     },
     yaxis: {
@@ -107,7 +101,6 @@
   };
 
 
-
   // ********************************************
   //  online section start
   // ********************************************
@@ -116,11 +109,16 @@
     request.open("GET", "/temperature", true);
     request.onload = function () {
       if (request.readyState == 4 && request.status == 200) {
-        setGauge(request.responseText); // set gauge value
+        // let value = request.responseText;
+        // setGauge(request.responseText); // set gauge value
+        // tempNum = parseFloat(value);
+        setGauge(parseFloat(request.responseText));
+        // console.log(tempNum);
       }
     };
     request.onerror = function () {
-      setGauge(tempStr); // set gauge value
+      // setGauge(tempStr); // set gauge value
+      setGauge(tempNum);
     };
     request.send();
   };
@@ -257,27 +255,27 @@
   // ********************************************
   const colorsArray = ["#07bbec", "#f6ffa3", "#e8ec07", "#ec8107", "#ec0707"];
   // для цвета: сделать массив значений, выборку делать делением текущего значения переменной на количество элементов нацело оператор (%)
-  let gaugeMode;
+  let gaugeMode = 1;
 
-  const setGaugeMode = (num = 2) => {
+  const setGaugeMode = (mode) => {
     // 1 for (-50 0 50)
     // 2 for (20 100)
-    gaugeMode = num;
-    // ************ automatic gaugeMode set start
-    if (tempNum >= 50) {
-
-      gaugeMode = 2;
-    } else if (tempNum <= 20) {
-      gaugeMode = 1;
-    }
+    gaugeMode = mode;
+    // // ************ automatic gaugeMode set start
+    // if (temp >= 50) {
+    //   gaugeMode = 2;
+    // }
+    // if (temp <= 20) {
+    //   gaugeMode = 1;
+    // }
     // ************ automatic gaugeMode set end
     const mFiftyToFifty = document.getElementById("numbers50-50");
     const twentyToHundred = document.getElementById("numbers20-100");
-    if (gaugeMode === 2) {
+    if (mode === 2) {
       mFiftyToFifty.setAttribute("opacity", 0);
       twentyToHundred.setAttribute("opacity", 1);
     }
-    if (gaugeMode === 1) {
+    if (mode === 1) {
       mFiftyToFifty.setAttribute("opacity", 1);
       twentyToHundred.setAttribute("opacity", 0);
     }
@@ -338,14 +336,21 @@
     gaugeMode === 2 ? setGaugeMode(1) : setGaugeMode(2);
   });
 
-  const setGauge = (num, mode = 2) => {
-    setGaugeMode(mode);
-    setGaugeThemperatureNumber(num);
-    setGaugeArrowAngle(num);
-    setGaugeArcLine(num);
+  const setGauge = (temp) => {
+    // ************ automatic gaugeMode set start
+    if (temp >= 50) {
+      setGaugeMode(2);
+    }
+    if (temp <= 20) {
+      setGaugeMode(1);
+    }
+    // setGaugeMode(mode);
+    setGaugeThemperatureNumber(temp);
+    setGaugeArrowAngle(temp);
+    setGaugeArcLine(temp);
   };
 
-  setGauge(tempNum, 2); // set gauge value
+  // setGauge(tempNum, 2); // set gauge value
   // ********************************************
   //  New gauge section end
   // ********************************************
